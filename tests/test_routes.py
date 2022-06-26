3c01561444bd18a40998763e73314d5d3bd1375d
Wensi Rong <wr2110@nyu.edu>
2022-06-26 18:54:45 -0400

    finished_story_6

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,5 @@
 """
-TestYourResourceModel API Service Test Suite
+TestInventory API Service Test Suite
 
 Test cases can be run with the following:
   nosetests -v --with-spec --spec-color
@@ -9,21 +9,32 @@ import os
 import logging
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
+from tests.factory import InventoryFactory, ProductFactory
 from service import app
-from service.models import db
+from service.models import db, Inventory, init_db
 from service.utils import status  # HTTP Status Codes
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
+)
+
+BASE_URL = "/inventories"
 
 
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
-class TestYourResourceServer(TestCase):
+class TestInventoryServer(TestCase):
     """ REST API Server Tests """
 
     @classmethod
     def setUpClass(cls):
         """ This runs once before the entire test suite """
-        pass
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        init_db(app)
 
     @classmethod
     def tearDownClass(cls):
@@ -32,11 +43,34 @@ class TestYourResourceServer(TestCase):
 
     def setUp(self):
         """ This runs before each test """
-        self.app = app.test_client()
+        db.session.query(Inventory).delete()  # clean up the last tests
+        db.session.commit()
+        self.client = app.test_client()
 
     def tearDown(self):
         """ This runs after each test """
-        pass
+        db.session.remove()
+
+
+    ######################################################################
+    #  H E L P E R   M E T H O D S
+    ######################################################################
+
+    def _create_inventories(self, count):
+        """Factory method to create inventories in bulk"""
+        inventories = []
+        for _ in range(count):
+            inventory = InventoryFactory()
+            resp = self.client.post(BASE_URL, json=inventory.serialize())
+            self.assertEqual(
+                resp.status_code,
+                status.HTTP_201_CREATED,
+                "Could not create test inventory",
+            )
+            new_inventory = resp.get_json()
+            inventory.id = new_inventory["id"]
+            inventories.append(inventory)
+        return inventories
 
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
@@ -44,5 +78,19 @@ class TestYourResourceServer(TestCase):
 
     def test_index(self):
         """ It should call the home page """
-        resp = self.app.get("/")
+        resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_list_inventory_list(self):
+        """It should Get a list of Inventories"""
+        # when no data, return []
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data, [],"wrong response")
+        # when there is 5 inventories, return 5 inventories
+        self._create_inventories(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
