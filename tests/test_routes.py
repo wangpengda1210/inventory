9b5b4b574b505993bc988e4c7b15f237d9346d5a
Daria Xu <dariaxu1997@outlook.com>
2022-06-27 23:31:32 -0400

    Need more test cases

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -9,7 +9,7 @@ import os
 import logging
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
-from tests.factory import InventoryFactory, ProductFactory
+from tests.factory import InventoryFactory, ProductFactory, Condition
 from service import app
 from service.models import db, Inventory, init_db
 from service.utils import status  # HTTP Status Codes
@@ -73,6 +73,20 @@ class TestInventoryServer(TestCase):
             inventories.append(inventory)
         return inventories
 
+    def _generate_inventories_with_products(self, num_inventories, num_products):
+        """Factory method to create inventories with products in bulk"""
+        inventories_json = []
+        for i in range(num_inventories):
+            inventory = InventoryFactory()
+
+            for _ in range(num_products):      
+                product = ProductFactory()
+                inventory.products.append(product)
+            # print(inventory.serialize())
+            inventories_json.append(inventory.serialize())
+
+        return inventories_json
+
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
     ######################################################################
@@ -95,3 +109,97 @@ class TestInventoryServer(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
+
+    def test_create_inventory(self):
+        """It should Create an Inventory """
+        # generate fake request json
+        requests_json = self._generate_inventories_with_products(2,1)
+
+        # create not conflict product in one inventory  
+        # set two inventories the same name 
+        requests_json[1]["name"] = requests_json[0]["name"]
+        # set the condition different
+        products_first = requests_json[0]["products"][0]
+        products_first["condition"] = Condition.NEW
+        # products_first["restock_level"] = "MODERATE"
+        products_second = requests_json[1]["products"][0]
+        products_second["condition"] = Condition.OPEN_BOX
+        # send the first request
+        # print(requests_json[0])
+        # print("firstffff")
+        resp = self.client.post(BASE_URL, json=requests_json[0])
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        # print(resp.get_json())
+        # print("first")
+        # send the second request
+        resp = self.client.post(BASE_URL, json=requests_json[1])
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        # print(resp.get_json())
+        # print("second")
+
+        # # create different inventory
+        requests_json[0]["name"] += "_diff"
+        resp = self.client.post(BASE_URL, json=requests_json[0])
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        # print(resp.get_json())
+
+
+    # ######################################################################
+    # #  T E S T   S A D   P A T H S
+    # ######################################################################
+
+    def test_create_inventory_no_data(self):
+        """It should not Create an Inventory with missing data"""
+        resp = self.client.post(BASE_URL, json={})
+        self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+        
+    def test_create_inventory_conflict(self):
+        """It should not Create two Inventory Product with same condition"""
+        # generate fake request json
+        requests_json = self._generate_inventories_with_products(2,1)
+
+        # create not conflict product in one inventory  
+        # set two inventories the same name 
+        requests_json[1]["name"] = requests_json[0]["name"]
+        # set the condition different
+        products_first = requests_json[0]["products"][0]
+        products_first["condition"] = Condition.NEW
+        products_second = requests_json[1]["products"][0]
+        products_second["condition"] = Condition.OPEN_BOX
+        # send the first request
+        resp = self.client.post(BASE_URL, json=requests_json[0])
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        print(resp.get_json())
+        # send the second request
+        resp = self.client.post(BASE_URL, json=requests_json[1])
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        print(resp.get_json())
+
+        # set a conflict condition
+        conflict_json = self._generate_inventories_with_products(1,2)[0]
+        conflict_products_first = conflict_json["products"][0]
+        conflict_products_first["condition"] = Condition.USED
+        conflict_products_second = conflict_json["products"][1]
+        conflict_products_second["condition"] = Condition.OPEN_BOX
+        conflict_json["name"] = requests_json[0]["name"]
+        print(conflict_json)
+        resp = self.client.post(BASE_URL, json=conflict_json)
+        self.assertEqual(resp.status_code, status.HTTP_409_CONFLICT)
+
+
+
+    def test_create_inventory_no_name(self):
+        """It should not Create an Inventory with no inventory name"""
+        requests_json = self._generate_inventories_with_products(1,1)[0]
+        requests_json.pop("name")
+        resp = self.client.post(BASE_URL, json=requests_json)
+        self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+    def test_create_inventory_bad_data(self):
+        """It should not Create an Inventory Product with dad data"""
+        # create inventory with incomplete product data
+        requests_json = self._generate_inventories_with_products(1,1)[0]
+        requests_json["products"][0].pop("condition")
+        resp = self.client.post(BASE_URL, json=requests_json)
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
