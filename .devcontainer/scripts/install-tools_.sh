e48a0519d9b79930ff5f71bf4d59727fb7c0ea2d
Wensi Rong <wr2110@nyu.edu>
2022-08-11 12:53:59 -0400

    v1

--- .devcontainer/scripts/install-tools_.sh
+++ .devcontainer/scripts/install-tools_.sh
@@ -8,25 +8,6 @@ echo "**********************************************************************"
 echo "Installing K3D Kubernetes..."
 echo "**********************************************************************"
 curl -s "https://raw.githubusercontent.com/rancher/k3d/main/install.sh" | sudo bash
-echo "Creating kc and kns alias for kubectl..."
-echo "alias kc='/usr/local/bin/kubectl'" >> $HOME/.bash_aliases
-echo "alias kns='kubectl config set-context --current --namespace'" >> $HOME/.bash_aliases
-
-echo "**********************************************************************"
-echo "Install Kustomize CLI..."
-echo "**********************************************************************"
-curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
-sudo mv kustomize /usr/local/bin/kustomize
-echo "Creating ku alias for kustomize..."
-echo "alias ku='/usr/local/bin/kustomize'" >> $HOME/.bash_aliases
-
-echo "**********************************************************************"
-echo "Install Tekton CLI..."
-echo "**********************************************************************"
-curl -LO https://github.com/tektoncd/cli/releases/download/v0.25.0/tkn_0.25.0_Linux_x86_64.tar.gz
-sudo tar xvzf tkn_0.25.0_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
-sudo ln -s /usr/local/bin/tkn /usr/bin/tkn
-rm tkn_0.25.0_Linux_x86_64.tar.gz 
 
 echo "**********************************************************************"
 echo "Installing IBM Cloud CLI..."
@@ -36,8 +17,12 @@ echo "source /usr/local/ibmcloud/autocomplete/bash_autocomplete" >> $HOME/.bashr
 # Install user mode tools
 ibmcloud plugin install container-service -r 'IBM Cloud'
 ibmcloud plugin install container-registry -r 'IBM Cloud'
-echo "Creating aliases for ibmcloud tools..."
+
+echo "Creating aliases for new tools..."
 echo "alias ic='/usr/local/bin/ibmcloud'" >> $HOME/.bash_aliases
+echo "alias kc='/usr/local/bin/kubectl'" >> $HOME/.bash_aliases
+echo "alias ku='/usr/local/bin/kustomize'" >> $HOME/.bash_aliases
+echo "alias kns='kubectl config set-context --current --namespace'" >> $HOME/.bash_aliases
 
 # Platform specific installs
 if [ $(uname -m) == aarch64 ]; then
