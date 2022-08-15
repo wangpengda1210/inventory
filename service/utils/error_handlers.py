b7a0fc0f3399179c7a6cad2f00f735149ad9fe46
Wentao Chen <wc2215@nyu.edu>
2022-08-15 11:18:46 +0000

    remove redundant error handlers

--- service/utils/error_handlers.py
+++ service/utils/error_handlers.py
@@ -75,18 +75,3 @@ def data_conflict(error):
         ),
         status.HTTP_409_CONFLICT,
     )
-
-
-@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
-def internal_server_error(error):
-    """Handles unexpected server error with 500_SERVER_ERROR"""
-    message = str(error)
-    app.logger.error(message)
-    return (
-        jsonify(
-            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            error="Internal Server Error",
-            message=message,
-        ),
-        status.HTTP_500_INTERNAL_SERVER_ERROR,
-    )
